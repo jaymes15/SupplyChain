2adc32acfccb6a5bf72347e2bae3de7d9242f996
jaymes15 <jamesoluwatomisinomotosho@gmail.com>
2022-05-30 11:07:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+0x9f92715CFF1F06fD6D8e4F60922733D94192d8eD
+# Supply Chain Smart Contract
+
+Written with Solidity and Javascript
+
+
+RINKEBY NETWORK:
+ - contract address: 0x2686b3CE41505052e0929dE877625D9BbFe96DD7
+
+ - Link to contract details on etherscan: https://rinkeby.etherscan.io/address/0x2686b3CE41505052e0929dE877625D9BbFe96DD7
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,12 @@
 const path = require("path");
+require('dotenv').config({path: __dirname + '/.env'});;
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const infuraRinkebyURL = process.env['RINKEBY_NETWORK'];
+const mnemonic = process.env['ACC_MNEMONIC'];
+
+console.log(`MMMMM ${mnemonic}`);
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,7 +15,12 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infuraRinkebyURL),
+      network_id: 4,
+      gas: 5500000,        
+    },
   },
   compilers: {
     solc: {
